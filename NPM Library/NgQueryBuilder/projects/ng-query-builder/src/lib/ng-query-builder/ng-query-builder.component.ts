6dc7f1397d065e9d10a45d086871f014ceaf8e08
Rohan Patel <42444158+gamble4846@users.noreply.github.com>
2024-03-05 00:49:23 +0530

    Works

--- NPM Library/NgQueryBuilder/projects/ng-query-builder/src/lib/ng-query-builder/ng-query-builder.component.ts
+++ NPM Library/NgQueryBuilder/projects/ng-query-builder/src/lib/ng-query-builder/ng-query-builder.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgQueryBuilderService } from './ng-query-builder.service';
+declare var jspreadsheet: any;
+declare var $:any;
 
 @Component({
   selector: 'lib-ng-query-builder',
@@ -8,10 +10,48 @@ import { NgQueryBuilderService } from './ng-query-builder.service';
 })
 export class NgQueryBuilderComponent {
   constructor(private _NgQueryBuilderService:NgQueryBuilderService){}
+  @ViewChild('NgQueryBuilderDIV') NgQueryBuilderDIV: any;
 
   ngOnInit(): void {
     this._NgQueryBuilderService.setupExternalFiles().subscribe((response: boolean) => {
-      console.log(response);
+      console.log(response, this.NgQueryBuilderDIV);
+      $(this.NgQueryBuilderDIV.nativeElement).queryBuilder({
+        filters: [ 
+          {
+            id: 'name',
+            field: 'username',
+            label: {
+              en: 'Name',
+              fr: 'Nom'
+            },
+            icon: 'bi-person-fill',
+            value_separator: ',',
+            type: 'string',
+            optgroup: 'core',
+            default_value: 'Mistic',
+            size: 30,
+            validation: {
+              allow_empty_value: true
+            },
+            unique: true
+          },
+          {
+            id: 'age',
+            label: 'Age',
+            icon: 'bi-calendar3',
+            type: 'integer',
+            input: 'text',
+            value_separator: '|',
+            optgroup: 'core',
+            description: (rule:any) => {
+              if (rule.operator && ['in', 'not_in'].indexOf(rule.operator.type) !== -1) {
+                return 'Use a pipe (|) to separate multiple values with "in" and "not in" operators';
+              }
+              return '';
+            }
+          },
+         ]
+      });
     });
   }
 }
