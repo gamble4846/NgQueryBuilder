373367dda46b7260681f66d729a34bd2c27695ef
Rohan Patel <42444158+gamble4846@users.noreply.github.com>
2024-03-05 01:25:58 +0530

    .

--- NPM Library/NgQueryBuilder/projects/ng-query-builder/src/lib/ng-query-builder/ng-query-builder.component.ts
+++ NPM Library/NgQueryBuilder/projects/ng-query-builder/src/lib/ng-query-builder/ng-query-builder.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgQueryBuilderService } from './ng-query-builder.service';
 declare var jspreadsheet: any;
-declare var $:any;
+declare var $: any;
 
 @Component({
   selector: 'lib-ng-query-builder',
@@ -9,14 +9,21 @@ declare var $:any;
   styleUrl: './ng-query-builder.component.css'
 })
 export class NgQueryBuilderComponent {
-  constructor(private _NgQueryBuilderService:NgQueryBuilderService){}
+  constructor(private _NgQueryBuilderService: NgQueryBuilderService) { }
   @ViewChild('NgQueryBuilderDIV') NgQueryBuilderDIV: any;
 
   ngOnInit(): void {
     this._NgQueryBuilderService.setupExternalFiles().subscribe((response: boolean) => {
       console.log(response, this.NgQueryBuilderDIV);
       $(this.NgQueryBuilderDIV.nativeElement).queryBuilder({
-        filters: [ 
+        icons: {
+          add_group: 'fas fa-plus-square',
+          add_rule: 'fas fa-plus',
+          remove_group: 'fas fa-minus-square',
+          remove_rule: 'far fa-trash-alt',
+          error: 'fas fa-exclamation-circle'
+        },
+        filters: [
           {
             id: 'name',
             field: 'username',
@@ -43,14 +50,14 @@ export class NgQueryBuilderComponent {
             input: 'text',
             value_separator: '|',
             optgroup: 'core',
-            description: (rule:any) => {
+            description: (rule: any) => {
               if (rule.operator && ['in', 'not_in'].indexOf(rule.operator.type) !== -1) {
                 return 'Use a pipe (|) to separate multiple values with "in" and "not in" operators';
               }
               return '';
             }
           },
-         ]
+        ]
       });
     });
   }
